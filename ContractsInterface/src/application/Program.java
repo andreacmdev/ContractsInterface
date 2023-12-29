ba9db368120d54e5197735ba5fc417966f33e335
Andre Machado <mmachadoacm@gmail.com>
2023-12-28 21:06:24 -0300

    create Interface Online Payment

--- ContractsInterface/src/application/Program.java
+++ ContractsInterface/src/application/Program.java
@@ -1,10 +1,39 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Contract;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Locale.setDefault(Locale.US);
+		Scanner scan = new Scanner (System.in);
+		
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		System.out.println("Entre os dados do Contrato");
+		System.out.println("Número: ");
+		int number = scan.nextInt();
+		System.out.println("Data (dd/MM/yyyy): ");
+		LocalDate date = LocalDate.parse(scan.next());
+		System.out.println("Valor do Contrato: ");
+		double TotalValue = scan.nextDouble();
+		
+		Contract contract = new Contract(number, date, TotalValue);
+		
+		System.out.print("Entre com o número de parcelas: ");
+		int n = scan.nextInt();
+		
+		
+		
+		
+		
+		scan.close();
 	}
 
 }
---
+++ ContractsInterface/src/services/ContractService.java
@@ -0,0 +1,19 @@
+package services;
+
+import entities.Contract;
+
+public class ContractService {
+	
+	private OnlinePaymentService onlinePaymentService;
+
+	public ContractService(OnlinePaymentService onlinePaymentService) {
+		this.onlinePaymentService = onlinePaymentService;
+	}
+	
+	public void processContract (Contract contract, int months) {
+		
+	}
+	
+	
+
+}
